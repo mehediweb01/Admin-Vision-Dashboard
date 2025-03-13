70feb8606b90ab1aefa2396b2a0d017f26626604
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-03-14 01:32:26 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Admin Vision Dashboard
+
+## Technologies
+
+- React
+- Tailwind CSS
+- material-ui (MUI)
+- Recharts
+- React Router
+- React Icons
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-sky-100/50">
+      <h1 className="font-bold font-serif text-4xl text-gray-700">
+        Welcome to admin vision dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
