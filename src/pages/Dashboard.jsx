22a7c607c609fd0a444cdf854f74bfd565a35fbe
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-03-15 02:06:34 +0600

    dashboard / kpi card section done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import KPICard from "../components/dashboard/KPICard";
 
 const Dashboard = () => {
+  const [kpi, setKpi] = useState([]);
+  useEffect(() => {
+    fetch("/src/db/data.json")
+      .then((res) => res.json())
+      .then((data) => setKpi(data.kpi));
+  });
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="font-bold font-serif text-4xl text-gray-700">
-        Welcome to admin vision dashboard
-      </h1>
-    </div>
+    <main className="my-8 me-4">
+      {/* kpi card start */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        {kpi.map((item) => (
+          <KPICard {...item} key={item.id} />
+        ))}
+      </div>
+      {/* kpi card end */}
+    </main>
   );
 };
 
