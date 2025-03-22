4f6105f4976d757453a6a5c7ade171447673da04
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-03-22 23:12:07 +0600

    invoice done

--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/shared/Navbar";
 import SideSection from "../components/shared/Sidebar/SideSection";
 import VisionCard from "../components/billing/VisionCard";
 import CreditBalance from "../components/billing/CreditBalance";
+import Invoice from "../components/billing/Invoice";
 
 const Billing = () => {
   return (
@@ -13,12 +14,14 @@ const Billing = () => {
       <div className="w-full">
         <Navbar />
         <div className="px-2 py-4">
-          <div className="flex md:flex-row flex-col gap-3 items-start sm:items-center">
+          <div className="flex md:flex-row flex-col gap-3 items-start">
             <div className="flex justify-between items-start gap-4 md:w-[60%] w-full lg:flex-row flex-col">
               <VisionCard />
               <CreditBalance />
             </div>
-            <div className="md:w-[40%] w-full"></div>
+            <div className="md:w-[40%] w-full">
+              <Invoice />
+            </div>
           </div>
         </div>
       </div>
